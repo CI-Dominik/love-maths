486235be557c25ee2a3f21c6a59f1ee13d629aae
Dominik Wulf <dominik.wulf@students.codeinstitute.net>
2024-08-15 16:48:07 +0200

    Add function basics to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// Run Game
+
+function runGame() {
+
+}
+
+// Check Answer
+
+function checkAnswer() {
+
+}
+
+// Calculate correct answers
+
+function calculateCorrectAnswer() {
+
+}
+
+// Increment score count
+
+function incrementScore() {
+
+}
+
+// Increment wrong answer count
+
+function incrementWrongAnswer() {
+
+}
+
+// Display addition question
+
+function displayAdditionQuestion() {
+
+}
+
+// Display subtract question
+
+function displaySubtractQuestion() {
+
+}
+
+// Display multiply question
+
+function displayMultiplyQuestion() {
+
+}
+
+// Display divide question
+
+function displayDivideQuestion() {
+
+}
