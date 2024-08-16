3efaf1c65be224dd97cda1d8ff5a96e6c7145283
Dominik Wulf <dominik.wulf@students.codeinstitute.net>
2024-08-16 12:15:48 +0200

    Adjust subtract and division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -117,8 +117,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 < operand2 ? operand1 : operand2;
     document.getElementById('operator').textContent = "-";
 
 }
@@ -133,8 +133,14 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivisionQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "/";
+    if (operand1 % operand2 === 0 || operand2 % operand1 === 0) {
+
+        document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+        document.getElementById('operand2').textContent = operand1 < operand2 ? operand1 : operand2;
+        document.getElementById('operator').textContent = "/";
+
+    } else {
+        runGame("division");
+    }
 
 }
